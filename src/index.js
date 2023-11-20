74cadd5ef20c83cf907549b3c336029d21bcd4a0
Lucas Lima <lucasbezerra14.ll@gmail.com>
2023-11-20 11:48:35 -0300

    Updating the folders architecture

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/settings/colors.css';
+import './styles/generic/reset.css';
+import './styles/elements/base.css';
+import './App.css';
 import App from './App';
-import './generic/reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
