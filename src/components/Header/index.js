65c0e49fecbdc64c8da2bc4eb46322021ffe519c
Lucas Lima <lucasbezerra14.ll@gmail.com>
2023-11-22 20:28:10 -0300

    Updatings CSS files for 'obejcts' and 'components'

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,8 @@
 import React from "react";
-
+import './styles.css';
 import Logo from '../../objects/Logo';
 import About from '../../objects/About';
 import Menu from '../../objects/Menu';
-
-import './styles.css';
 
 const Header = () => {
     return (
